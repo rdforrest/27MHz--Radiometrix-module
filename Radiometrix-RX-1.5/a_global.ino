425490442e88ba7a1921d34f7fab5584e78572b9
David Forrest <33370970+rdforrest@users.noreply.github.com>
2023-01-12 10:32:29 +0000

    Delete Radiometrix-RX-1.5 directory

--- Radiometrix-RX-1.5/a_global.ino
+++
@@ -1,9 +0,0 @@
-unsigned int ppmValues[8];
-int pulse = 0;
-int rssi;
-int incomingData = 0; // for incoming serial data
-int i = 0; // loop counter
-byte byteArr[8];
-
-// Test values for servo outputs
-int buf[6] = {20, 75, 100, 150, 200, 255}; // Test values for servo outputs
