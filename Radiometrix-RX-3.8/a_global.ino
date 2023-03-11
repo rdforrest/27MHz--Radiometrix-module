3d1ce8a123e2be1af9949176a19bb77a5c0d8011
David Forrest <33370970+rdforrest@users.noreply.github.com>
2023-03-11 08:57:16 +0000

    Add files via upload

---
+++ Radiometrix-RX-3.8/a_global.ino
@@ -0,0 +1,7 @@
+int pulse = 0;
+int rssi;
+int incomingData = 0; // for incoming serial data
+int i = 0; // loop counter
+int counter = 0;
+
+#define DEBUG 1; // Comment this out to blank out debug code
---
+++ Radiometrix-RX-3.8/c_setup.ino
@@ -0,0 +1,61 @@
+#include <avr/wdt.h>
+#include <Manchester.h>
+#include <Servo.h>
+
+// create servo object to control a servo. twelve servo objects can be created on most boards
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+
+#define RX_PIN 11
+#define LED_PIN 13
+#define CHAN2_ONOFF 4
+
+#define BUFFER_SIZE 7
+uint8_t buffer[BUFFER_SIZE];
+
+// FAILSAFE values
+int val1 = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+int val5 = 0;
+int val6 = 0;
+
+// Blinky on receipt
+#define LED 13
+
+unsigned long previousMillis = 0;        // for time delay used in Failsafe
+const long interval = 10000;           // set time interval (milliseconds)
+unsigned long currentMillis = millis();
+
+void setup()
+{
+  servo1.attach(5, 1000, 2000); // Set Servo Min and Max
+  servo2.attach(6, 1000, 2000);
+  servo3.attach(7, 1000, 2000);
+  servo4.attach(8, 1000, 2000);
+  servo5.attach(9, 1000, 2000);
+  servo6.attach(10, 1000, 2000);
+
+  pinMode(5, INPUT); // Data input
+  pinMode(6, OUTPUT);// Not used
+  pinMode(8, INPUT); // Not used
+
+  Serial.begin(9600);  // RDF was 115200
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("RX start");
+  delay(120);
+
+  pinMode(CHAN2_ONOFF, OUTPUT);
+  pinMode(LED, OUTPUT);
+ Serial.begin(9600);
+  man.setupReceive(RX_PIN, MAN_300); // Set pin and speed
+  man.beginReceiveArray(BUFFER_SIZE, buffer);
+
+}
