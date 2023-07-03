1dd3f596e7fd552cbb2d3cc6f978964f6688bab7
David Forrest <33370970+rdforrest@users.noreply.github.com>
2023-07-03 12:50:55 +0100

    Delete a_global.ino

--- a_global.ino
+++
@@ -1,6 +0,0 @@
-unsigned int ppmValues[8];
-int pulse = 0;
-int i = 0; // loop counter
-long TX_previousMillis = 0; // last time updated
-long TX_interval = 250; // Interval - our delay interval in milliseconds
-#define DEBUG 1; // Comment this out to blank out debug code
