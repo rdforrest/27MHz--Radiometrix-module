ec5ce5d5aeda33fc6149c7703e4fb8718a406989
David Forrest <33370970+rdforrest@users.noreply.github.com>
2025-06-07 11:30:49 +0100

    Delete SD-card-data-logger-RDF-1.3 directory

--- SD-card-data-logger-RDF-1.3/a_global.ino
+++
@@ -1,8 +0,0 @@
-#include <OneWire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <Wire.h>
-#include "RTClib.h"
-const int chipSelect = 10 ; //cs or the save select pin from the sd shield is connected to 10.
-RTC_DS1307 RTC;
-float celsius, fahrenheit;
